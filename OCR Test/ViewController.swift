811d9565bcb62cb82ea5435c242af6b6f9519d00
Ozer Gul <ozer.gul@cdev.global>
2019-10-24 12:04:04 +0300

    Initial Commit

---
+++ OCR Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OCR Test
+//
+//  Created by CDEV-TURKEY on 24.10.2019.
+//  Copyright © 2019 CDEV-TURKEY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
